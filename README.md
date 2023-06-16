a2d290baf534eaf37841b1efdbbcb23a7b7aa86b
PSY222 <ss26348@naver.com>
2023-06-16 16:59:00 +0900

    argparse simplified, edited ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Automated-Reference-List-Generator
-Quick and simple reference list generator that extracts information from pdf files inside a given folder.
+Quick and simple reference list generator that extracts information from pdf files inside a given folder. 
+
+## Quick facts about this generator
+- This generator automatically extracts information from pdf files located inside a given [folder path].
+- Current reference format follows IEEE format like "[reference number] {author}. {title}, {published year}"
+- (Feel free to change the format to APA,MLA, Harvard Referencing format or etc.)
+
+### Expected out comes
+```
+References
+[1] Pedro Martins, José Silvestre Silva and Alexandre Bernardino. Multispectral Facial Recognition in the Wild, 2022 ```
+
+## 📌Limitations
+- Author and title are extracted based on the first/second line of the document unless proper metadata is provided. Therefore, it is important to double-check the details after generating the reference draft using this generator.
+- If your title is over one line, you should edit the title.
+- It was challenging to extract publication information such as volume, issue, or other relevant details from the documents due to the varying formats of research papers.
+- But, I am sure that this can be very simple but useful reference list generator for those who keep the referred PDF documents to the folder 📚
+
+## How to use this repo 🖐
+1. git clone [this repo HTTPS code]
+2. Navigate to this repo
+```cd Automated-Reference-List-Generator```
+3. Type the command
+```python pdf2ref.py -f [folder path] -o [output path to save txt file] ```
--- pdf2ref.py
+++ pdf2ref.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import PyPDF2
 
 def extract_info_from_pdf(file_path):
@@ -7,11 +8,12 @@ def extract_info_from_pdf(file_path):
         meta = pdf.metadata
         title = meta.title
         author = meta.author
+        date = meta.creation_date if meta.creation_date else " date unknown"
         if not title:
             title = pdf.pages[0].extract_text().split('\n')[0].strip()
         if not author: 
             author = pdf.pages[0].extract_text().split('\n')[1].strip()
-        return title, author
+        return title, author, date
     
 def list_paper_references(folder_path, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
@@ -21,13 +23,35 @@ def list_paper_references(folder_path, output_file):
         for file_name in files:
             file_path = os.path.join(folder_path, file_name)
             if file_name.endswith('.pdf'):
-                title, author = extract_info_from_pdf(file_path)
-                if title and author:
+                title, author, date = extract_info_from_pdf(file_path)
+                if title and author and date:
                     cnt += 1
-                    reference = f"[{cnt}] {author}. {title}."
+                    reference = f"[{cnt}] {author}. {title}, {str(date)[:4]}"
                     file.write(reference + '\n')
 
-# Provide the folder path where the PDFs are located
-folder_path = 'C:/Users/ss263/Desktop/thesis/FA-GAN/example'
-output_file = 'C:/Users/ss263/Desktop/thesis/FA-GAN/example/output.txt'
-list_paper_references(folder_path, output_file)
+
+parser = argparse.ArgumentParser(description="parameters for reference list generation")
+
+parser.add_argument('-f',
+                    '--folder_path',
+                    help="Please enter the folder path where the research paper PDF file is located",
+                    type=str,
+                    required = True)
+
+parser.add_argument('-o',
+                    '--output_path',
+                    help="Please enter the path where txt file with reference list will be saved",
+                    type=str,
+                    required=True)
+
+args = parser.parse_args()
+
+try:
+  assert os.path.isdir(args.folder_path), "Input path does not exist"
+  assert os.path.isdir(os.path.dirname(args.output_path)), 'Output path directory does not exist'
+  list_paper_references(args.folder_path, args.output_path)
+except AssertionError as e:
+  print(str(e))
+  print("Check the folder path and output path")
+
+
